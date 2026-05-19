000724d4273919084af73f4fa7d662456c71b07c
parkcu71321 <parkcu71321@gmail.com>
2026-05-19 10:12:22 +0900

    7장 완성

--- src/main/java/kr/ac/kopo/pcu/bookmarket/controller/BookController.java
+++ src/main/java/kr/ac/kopo/pcu/bookmarket/controller/BookController.java
@@ -1,13 +1,22 @@
 package kr.ac.kopo.pcu.bookmarket.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import kr.ac.kopo.pcu.bookmarket.domain.Book;
 import kr.ac.kopo.pcu.bookmarket.service.BookService;
+import org.apache.juli.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -17,6 +26,9 @@ import java.util.Set;
 public class BookController {
     @Autowired
     private BookService bookService;
+
+    @Value("${file.uploadDir}")
+    String fileDir;
 
     @RequestMapping(method = RequestMethod.GET)
     public String requestBookList(Model model){
@@ -46,6 +58,54 @@ public class BookController {
         return "books";
     }
 
+    @GetMapping("/add")
+    public String requestAddBookForm(){
+        return "addBook";
+    }
+
+    @PostMapping("/add")
+    public String submitAddNewBook(@ModelAttribute Book book){
+        MultipartFile bookImage = book.getBookImage();
+        System.out.println("파일사이즈" + bookImage.getSize());
+        String saveName = bookImage.getOriginalFilename();
+        File saveFile = new File(fileDir, saveName);
+        if (bookImage != null && !bookImage.isEmpty()){
+            try {
+                bookImage.transferTo(saveFile);
+            } catch (IOException e) {
+                throw new RuntimeException("이미지가 업로드 되지 않았습니다.");
+            }
+        }
+        book.setFileName(saveName);
+        bookService.setNewBook(book);
+        return "redirect:/books";
+    }
+
+    @ModelAttribute
+    public void addAddtributes(Model model){
+        model.addAttribute("addTitle", "신규 도서 등록");
+    }
+
+
+    @GetMapping("/download")
+    public void downloadBookImage(@RequestParam("file") String paramKey, HttpServletResponse response){
+        File imgFile = new File(fileDir + paramKey);
+
+        response.setContentType("application/download");
+        response.setContentLength((int)imgFile.length());
+        response.setHeader("Content-Disposition", "attachment;filename=\"" + paramKey + "\"");
+
+        try {
+            OutputStream out = response.getOutputStream();
+            FileInputStream fileIn = new FileInputStream(imgFile);
+            FileCopyUtils.copy(fileIn, out);
+            fileIn.close();
+            out.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
     @GetMapping("/all")
     public ModelAndView requestAllBooks(){
--- src/main/java/kr/ac/kopo/pcu/bookmarket/domain/Book.java
+++ src/main/java/kr/ac/kopo/pcu/bookmarket/domain/Book.java
@@ -3,6 +3,7 @@ package kr.ac.kopo.pcu.bookmarket.domain;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
 
@@ -11,14 +12,16 @@ import java.math.BigDecimal;
 @Getter
 
 public class Book {
-    private String bookId; // 도서 ID
-    private String name; // 도서제목
-    private BigDecimal unitPrice; // 가격
-    private String author; // 저자
-    private String description; // 설명
-    private String publisher; // 출판사
-    private String category; // 분류
-    private long unitsInStock; // 재고수
-    private String releaseDate; // 출판일
-    private String condition; // 신규도서, 중고도서, E-Book
+    private String bookId;//도서 ID
+    private String name;//도서제목
+    private BigDecimal unitPrice;//가격
+    private String author;//저자
+    private String description;//설명
+    private String publisher;//출판사
+    private String category;//분류
+    private long unitsInStock;//재고수
+    private String releaseDate;//출판일
+    private String condition;//신규도서, 중고도서, E-Book
+    private String fileName; //도서이미지파일이름
+    private MultipartFile bookImage; //도서이미지파일정보객체
 }
